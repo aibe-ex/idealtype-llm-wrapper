3ebb0a79782b6d272ed27223e2566d79b0599083
dochmai382 <dochmai382@gmail.com>
2025-03-20 14:07:35 +0900

    express

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-// console.log("Hello World");
-console.log("Bye Earth");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    // res.send('Hello World!')
+    res.send('Good job!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
